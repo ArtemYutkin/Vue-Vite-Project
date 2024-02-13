9019cfb243a217fcfe9ffb7ee88042f9603aef5a
ArtemY <artemisko12345@gmail.com>
2024-02-13 16:15:56 +0700

    added counter

---
+++ src/components/BlockMount.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { reactive, ref } from 'vue';
+// const count = ref(0);
+const counter = reactive({
+    count: 100
+});
+const Plus = () => {
+    counter.count+=5
+}
+</script>
+
+<template>
+    <button @click="Plus()">
+        Count:
+        <span>{{ counter.count }}</span>
+    </button>
+    <button @click="counter.count = 100">
+        reset
+    </button>
+</template>
